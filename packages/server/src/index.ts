ada326dc0fa59fec2432e3d2bb2de708988475aa
undefined <i@undefined.moe>
2026-05-06 18:10:11 +0000

    remove prisma

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,18 +1,19 @@
 import { loadConfig } from './config.js';
 import { createApp } from './app.js';
-import { disconnectPrisma } from './db/prisma.js';
+import { destroyDb, migrateDb } from './db/database.js';
 import { disconnectRedis } from './redis/client.js';
 import { closeWorkers } from './voice/media-worker.js';
 
 async function main() {
   const config = loadConfig();
+  await migrateDb();
   const { fastify, turnTimer } = await createApp(config);
 
   const shutdown = async () => {
     turnTimer.stopAll();
     await closeWorkers();
     await fastify.close();
-    await disconnectPrisma();
+    await destroyDb();
     await disconnectRedis();
     process.exit(0);
   };
